ee61b6b412438576a41366a94196c61531454597
Dmytro Murashko <kein.azure@protonmail.com>
2025-01-24 15:12:55 +0200

    remove global font reset

--- src/core/providers/UiKitProvider/styles.tsx
+++ src/core/providers/UiKitProvider/styles.tsx
@@ -14,12 +14,6 @@ export const UIStyles = styled.div`
     box-sizing: border-box;
   }
 
-  // CSS resets to avoid inheriting from other other libraries e.g. antd.
-  & * {
-    font-size: ${({ theme }) => theme.typography.body.fontSize};
-    line-height: 1.5;
-  }
-
   & pre {
     ${({ theme }) => theme.typography.body}
   }
